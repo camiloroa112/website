73497968c0217f4beeec2eda3cf6160e2fdd7ee2
camiloroa112 <camiloroa112@gmail.com>
2024-04-17 22:39:37 -0500

    Creation of contact section.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 let tl = gsap.timeline({defaults: {duration: 0.75}})
 tl.fromTo('body', {opacity: 0, y: -50}, {opacity: 1, y: 0})
-tl.fromTo('#proyectos h1', {opacity: 0, x: -50}, {opacity: 1, x: 0})
+tl.fromTo('#projects h1', {opacity: 0, x: -50}, {opacity: 1, x: 0})
 tl.fromTo('.profile', {opacity: 0, x: 100}, {opacity: 1, x: 0})
